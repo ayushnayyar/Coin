6def761d0cc0913262bcdd6c00a3a273ae19bcb4
ayushnayyar <ayushnayyar48@gmail.com>
2021-11-05 01:43:40 +0530

    - Completed Left Nav Section

--- src/components/home/NavSection.js
+++ src/components/home/NavSection.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 
 import Avatar from '../../assets/images/man.png';
 
 import '../../common/home/nav-section.scss';
 
 const NavSection = () => {
+  const [isLinkActive, setLinkActive] = useState('Home');
+
+  const navLinks = [
+    { name: 'Home', to: '/' },
+    { name: 'People', to: '/people' },
+    { name: 'Photos', to: '/photos' },
+    { name: 'Profile', to: '/profile' },
+  ];
   return (
-    <div className="NavSection__Card">
+    <section className="NavSection__Card">
       <div className="NavSection__ProfileCard">
         <div className="NavSection__ProfileCard-image">
           <img src={Avatar} />
@@ -18,7 +27,82 @@ const NavSection = () => {
           </div>
         </div>
       </div>
-    </div>
+
+      <div className="NavSection__Nav">
+        {navLinks.map((link) => (
+          <div
+            onClick={() => {
+              setLinkActive(link.name);
+            }}
+            className={`${
+              isLinkActive === link.name
+                ? 'NavSection__Link-active'
+                : 'NavSection__Link'
+            }`}
+            key={link.name}
+          >
+            <div className="NavSection__Link-box">
+              <NavLink
+                activeClassName="NavSection__Link-text-active"
+                className="NavSection__Link-text"
+                exact
+                to={link.to}
+              >
+                {link.name}
+              </NavLink>
+            </div>
+          </div>
+        ))}
+        {/* <div className="NavSection__Link">
+          <div className="NavSection__Link-box">
+            <NavLink
+              activeClassName="NavSection__Link-text-active"
+              className="NavSection__Link-text"
+              exact
+              to="/"
+            >
+              Home
+            </NavLink>
+          </div>
+        </div>
+        <div className="NavSection__Link">
+          <div className="NavSection__Link-box">
+            <NavLink
+              activeClassName="NavSection__Link-text-active"
+              className="NavSection__Link-text"
+              exact
+              to="/"
+            >
+              People
+            </NavLink>
+          </div>
+        </div>
+        <div className="NavSection__Link">
+          <div className="NavSection__Link-box">
+            <NavLink
+              activeClassName="NavSection__Link-text-active"
+              className="NavSection__Link-text"
+              exact
+              to="/"
+            >
+              Photos
+            </NavLink>
+          </div>
+        </div>
+        <div className="NavSection__Link">
+          <div className="NavSection__Link-box">
+            <NavLink
+              activeClassName="NavSection__Link-text-active"
+              className="NavSection__Link-text"
+              exact
+              to="/"
+            >
+              Profile
+            </NavLink>
+          </div>
+        </div> */}
+      </div>
+    </section>
   );
 };
 
