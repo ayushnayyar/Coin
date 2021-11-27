d0c1d236d9e68b2f5685a112701684e5fb7b1b9e
ayushnayyar <ayushnayyar48@gmail.com>
2021-11-27 10:51:42 +0530

    - Added Create Post functionality

--- src/api/index.js
+++ src/api/index.js
@@ -3,3 +3,6 @@ import axios from 'axios';
 export const fetchPosts = () => {
   return axios.get(`${process.env.REACT_APP_BACKEND_URL}/posts`);
 };
+
+export const createPost = (post) =>
+  axios.post(`${process.env.REACT_APP_BACKEND_URL}/posts`, post);
--- src/components/home/CreatePost.js
+++ src/components/home/CreatePost.js
@@ -1,47 +1,73 @@
 import React, { useState, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 import Avatar from '../../assets/images/man.png';
 import Plus from '../../assets/icons/Plus';
 
+import { createPost } from '../../actions/posts';
 import '../../common/home/create-post.scss';
 
 const CreatePost = () => {
+  const dispatch = useDispatch();
+
   const [rows, setRows] = useState(1);
-  const [value, setValue] = useState('');
+  // const [postText, setPostText] = useState('');
+  const [postData, setPostData] = useState({
+    creator: 'Ayush Nayyar',
+    description: '',
+    tags: '',
+    selectedFile: '',
+  });
 
   useEffect(() => {
-    const rowlen = value.split('\n');
+    const rowlen = postData.description.split('\n');
 
     if (rowlen.length >= 1) {
       setRows(rowlen.length);
     }
-  }, [value]);
+  }, [postData.description]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    dispatch(createPost(postData));
+  };
 
   return (
     <div className="Create__Post">
       <div className="Create__Post__Logo">
         <img src={Avatar} />
       </div>
-      <div className="Create__Post__TextField">
-        <textarea
-          onChange={(text) => {
-            setValue(text.target.value);
-          }}
-          rows={rows}
-          type="text"
-          placeholder=" What's new, Ayush?"
-        />
-      </div>
-      <div className="Create__Post__Button">
-        <button
-          onClick={() => {
-            console.log('Clicked');
-          }}
-        >
-          <Plus />
-          Create
-        </button>
-      </div>
+      <form
+        className="Create__Post__Form"
+        autoComplete="off"
+        noValidate
+        onSubmit={(event) => handleSubmit(event)}
+      >
+        <div className="Create__Post__TextField">
+          <textarea
+            onChange={(text) => {
+              setPostData({ ...postData, description: text.target.value });
+              // setPostText(text.target.value);
+            }}
+            value={postData.description}
+            rows={rows}
+            type="text"
+            placeholder=" What's new, Ayush?"
+          />
+        </div>
+        <div className="Create__Post__Button">
+          <button
+            type="submit"
+            onClick={() => {
+              console.log('Clicked');
+            }}
+          >
+            <Plus />
+            Create
+          </button>
+        </div>
+      </form>
     </div>
   );
 };
